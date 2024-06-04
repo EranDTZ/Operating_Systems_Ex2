c42b11bc1e1694c2fe26bd9928fd88bed7ba8ea0
EranDTZ <tzarum77@gmail.com>
2024-06-04 20:48:51 +0300

    ttt improve

--- ttt.c
+++ ttt.c
@@ -1,13 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define N 3
 
 void print_error_and_exit() {
     printf("Error\n");
     exit(1);
 }
 
+void print_board(int board[9]){
+    for (int i = 0; i < 9; i++)
+    {
+        if (i == 2 || i == 5 || i == 8)
+        {
+            if (board[i] == 0)
+            {
+                printf("  ");
+                printf("\n");
+            }
+            if (board[i] == 1)
+            {
+                printf("X ");
+                printf("\n");
+            }
+            if (board[i] == 2)
+            {
+                printf("O ");
+                printf("\n");
+            }
+        }
+        else {
+            if (board[i] == 0) printf("  | ");
+            if (board[i] == 1) printf("X | ");
+            if (board[i] == 2) printf("O | ");
+        }
+    }
+}
+
+int board_full(int board[9]){
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[i]==0)
+        {
+            return 1;
+        }
+        
+    }
+    return 0; 
+}
+
 int main(int argc, char *argv[]) {
+
     if (argc != 2) {
         print_error_and_exit();
     }
@@ -17,41 +60,62 @@ int main(int argc, char *argv[]) {
         print_error_and_exit();
     }
 
-    int used[10] = {0}; // To keep track of used digits
+
     for (int i = 0; i < 9; ++i) {
-        if (strategy[i] < '1' || strategy[i] > '9' || used[strategy[i] - '0']) {
+        if (strategy[i] < '1' || strategy[i] > '9') {
             print_error_and_exit();
         }
-        used[strategy[i] - '0'] = 1;
     }
 
-    int board[9] = {0}; // 0 - empty, 1 - AI, 2 - Human
+    int board[9] = {0}; // empty = 0, AI = 1, Human = 2
     int moves = 0;
 
     while (moves < 9) {
-        int ai_move = -1;
+        int AI_move = -1;
         for (int i = 0; i < 9; ++i) {
             int pos = strategy[i] - '1';
+            //Chack if strategy MSD is empty - if so AI_move = pos - else AI_move = LSD
             if (board[pos] == 0) {
-                ai_move = pos;
+                AI_move = pos;
                 break;
             }
         }
-        if (ai_move == -1) {
-            ai_move = strategy[8] - '1';
+
+        //If the board almost full - LSD
+        if (AI_move == -1) {
+            AI_move = strategy[8] - '1';
         }
-        board[ai_move] = 1;
-        printf("%d\n", ai_move + 1);
+
+
+        board[AI_move] = 1;
         moves++;
 
+        //Chack if AI wins for all combinitions.
         if (moves >= 5) {
-            // Check for AI win (not necessary in a losing strategy)
+            if (board[0]==1&&board[1]==1&&board[2]==1 || board[3]==1&&board[4]==1&&board[5]==1 || board[6]==1&&board[7]==1&&board[8]==1)
+            {
+                printf("AI win!\n");
+                break;
+            }
+            if (board[0]==1&&board[3]==1&&board[6]==1 || board[1]==1&&board[4]==1&&board[7]==1 || board[2]==1&&board[5]==1&&board[8]==1)
+            {
+                printf("AI win!\n");
+                break;
+            }
+            if (board[0]==1&&board[4]==1&&board[8]==1 || board[2]==1&&board[4]==1&&board[6]==1)
+            {
+                printf("AI win!\n");
+                break;
+            }
         }
 
-        if (moves == 9) {
+        //If the board all full then its a DRAW
+        if (moves == 9 || !board_full) {
             printf("DRAW\n");
             break;
         }
+        
+        print_board(board);
 
         int human_move;
         scanf("%d", &human_move);
@@ -61,10 +125,27 @@ int main(int argc, char *argv[]) {
         board[human_move - 1] = 2;
         moves++;
 
-        if (moves >= 5) {
-            // Check for human win
+        //Chack if Human wins for all combinitions.
+        if (moves > 5) {
+            if (board[0]==2&&board[1]==2&&board[2]==2 || board[3]==2&&board[4]==2&&board[5]==2 || board[6]==2&&board[7]==2&&board[8]==2)
+            {
+                printf("YOU win!\n");
+                break;
+            }
+            if (board[0]==2&&board[3]==2&&board[6]==2 || board[1]==2&&board[4]==2&&board[7]==2 || board[2]==2&&board[5]==2&&board[8]==2)
+            {
+                printf("YOU win!\n");
+                break;
+            }
+            if (board[0]==2&&board[4]==2&&board[8]==2 || board[2]==2&&board[4]==2&&board[6]==2)
+            {
+                printf("YOU win!\n");
+                break;
+            }
         }
     }
+
+    print_board(board);
 
     return 0;
 }
