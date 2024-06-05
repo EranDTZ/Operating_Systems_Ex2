e4d461e402e68f50c107c6452bcbc6b0c830d5be
EranDTZ <tzarum77@gmail.com>
2024-06-05 19:41:51 +0300

    need help

--- mync_tcp.c
+++ mync_tcp.c
@@ -1,123 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <arpa/inet.h>
 
-// Function to handle TCP server input redirection
-void handle_tcp_input(int sockfd, FILE *output) {
-    char buffer[1024];
-    int n;
 
-    // Read from the connection and write to output
-    while ((n = read(sockfd, buffer, sizeof(buffer) - 1)) > 0) {
-        buffer[n] = '\0';
-        fprintf(output, "%s", buffer);
-        fflush(output);
+int tcpServer(int port)
+{
+    struct sockaddr_in server;
+    struct sockaddr_in client;
+    socklen_t client_len = sizeof(client);
+
+    memset(&server, 0, sizeof(server));
+    memset(&client, 0, sizeof(client));
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock < 0)
+    {
+        perror("socket(2)");
+        return 1;
     }
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    if (bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        perror("bind(2)");
+        close(sock);
+        return 1;
+    }
+
+    if (listen(sock, 1) < 0)
+    {
+        perror("listen(2)");
+        close(sock);
+        return 1;
+    }
+
+    printf("Waiting for TCP connection...\n");
+    int client_sock = accept(sock, (struct sockaddr *)&client, &client_len); // try to connect
+    if (client_sock < 0)
+    {
+        perror("accept(2)");
+        close(sock);
+        return 1;
+    }
+    return client_sock;
 }
 
-// Function to handle TCP client output redirection
-void handle_tcp_output(int sockfd, FILE *input) {
-    char buffer[1024];
+// פונקציית לקוח
+int tcpClient(int port, char *clientData)
+{
+    struct sockaddr_in server;
+    memset(&server, 0, sizeof(server));
+    struct addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET_ADDRSTRLEN];
 
-    // Read from input and write to the connection
-    while (fgets(buffer, sizeof(buffer), input) != NULL) {
-        send(sockfd, buffer, strlen(buffer), 0);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(clientData, NULL, &hints, &res)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
     }
+
+    printf("IP addresses for %s:\n\n", clientData);
+
+    for (p = res; p != NULL; p = p->ai_next)
+    {
+        void *addr;
+        char *ipver;
+
+        // get the pointer to the address itself
+        struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+        addr = &(ipv4->sin_addr);
+        ipver = "IPv4";
+
+        // convert the IP to a string and print it:
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("  %s: %s\n", ipver, ipstr);
+    }
+
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock < 0)
+    {
+        perror("socket(2)");
+        return 1;
+    }
+
+    if (inet_pton(AF_INET, ipstr, &server.sin_addr) <= 0)
+    {
+        perror("inet_pton(3)");
+        close(sock);
+        return 1;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+
+    printf("Connecting to Receiver...\n");
+    int con = connect(sock, (struct sockaddr *)&server, sizeof(server));
+    if (con < 0)
+    {
+        perror("connect(2)");
+        close(sock);
+        return 1;
+    }
+
+    freeaddrinfo(res); // free the linked list
+
+    return sock;
 }
 
-void start_tcp_server(int port, FILE *input, FILE *output) {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
+/*---------------------------------------------------------------------------------------------------------------------*/
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
 
-    // Handle input and output redirection
-    if (input) {
-        dup2(new_socket, STDIN_FILENO);
-        handle_tcp_input(new_socket, output);
-    }
-    if (output) {
-        dup2(new_socket, STDOUT_FILENO);
-        handle_tcp_output(new_socket, input);
-    }
 
-    close(new_socket);
-    close(server_fd);
-}
 
-void start_tcp_client(const char *host, int port, FILE *input, FILE *output) {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
-        exit(EXIT_FAILURE);
-    }
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-
-    if (inet_pton(AF_INET, host, &serv_addr.sin_addr) <= 0) {
-        perror("Invalid address / Address not supported");
-        exit(EXIT_FAILURE);
-    }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connection Failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Handle input and output redirection
-    if (input) {
-        dup2(sock, STDIN_FILENO);
-        handle_tcp_input(sock, output);
-    }
-    if (output) {
-        dup2(sock, STDOUT_FILENO);
-        handle_tcp_output(sock, input);
-    }
-
-    close(sock);
-}
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]){
     // Check if bad syntax, exit(1)
     if (argc < 5 || strcmp(argv[1], "-e") != 0) {
         printf("Error: bad syntax\n");
@@ -127,32 +135,53 @@ int main(int argc, char *argv[]) {
 
     //For adding the ./ to run the program
     char *run = "./";
-    int size = (sizeof(argv[2]) + sizeof(argv[3])) + 2;
+    int size = (sizeof(argv[2]) + sizeof(argv[3])) + 3;
     char *cmd = (char*)malloc(sizeof(char)*size);
     //Add the ./ befor the command
     strcpy(cmd,run);
     //Attach the command after the ./ run command
     strcat(cmd,argv[2]);
+    strcat(cmd," ");
     strcat(cmd,argv[3]);
 
     char *flag = argv[4];
     char *param = argv[5];
+    int sock;
 
     // Execute the command with input/output redirection
     if (strncmp(flag, "-i", 2) == 0 && strncmp(param, "TCPS", 4) == 0) {
         int port = atoi(param + 4);
-        start_tcp_server(port, NULL, stdout);
-    } else if (strncmp(flag, "-o", 2) == 0 && strncmp(param, "TCPC", 4) == 0) {
+        sock = tcpServer(port);
+        if (sock < 0)
+        {
+            fprintf(stderr, "Failed to start server on port %d\n", port);
+            exit(1);
+        }
+    }
+    else if (strncmp(flag, "-o", 2) == 0 && strncmp(param, "TCPC", 4) == 0) {
         char *host = strtok(param + 4, ",");
         int port = atoi(strtok(NULL, ","));
-        start_tcp_client(host, port, stdin, NULL);
-    } else if (strncmp(flag, "-b", 2) == 0 && strncmp(param, "TCPS", 4) == 0) {
-        int port = atoi(param + 4);
-        start_tcp_server(port, stdin, stdout);
-    } else {
+        sock = tcpClient(port, host);
+        if (sock < 0)
+        {
+            fprintf(stderr, "Failed to connect to server at %s:%d\n", host, port);
+            exit(1);
+        }
+    } 
+    else {
         fprintf(stderr, "Unsupported flag or parameter\n");
         exit(1);
     }
 
+    close(sock);
     return 0;
 }
+
+
+
+// ./mync_tcp -e “ttt 123456789” -i TCPS4455
+// ./mync_tcp -e “ttt 123456789” -o TCPC127.0.0.1,4455
+
+
+
+
---
+++ t_mync_tcp.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+// Function to handle TCP server input redirection
+void handle_tcp_input(int sockfd, FILE *output) {
+    char buffer[1024];
+    int n;
+
+    // Read from the connection and write to output
+    while ((n = read(sockfd, buffer, sizeof(buffer) - 1)) > 0) {
+        buffer[n] = '\0';
+        fprintf(output, "%s", buffer);
+        fflush(output);
+    }
+}
+
+// Function to handle TCP client output redirection
+void handle_tcp_output(int sockfd, FILE *input) {
+    char buffer[1024];
+
+    // Read from input and write to the connection
+    while (fgets(buffer, sizeof(buffer), input) != NULL) {
+        send(sockfd, buffer, strlen(buffer), 0);
+    }
+}
+
+void start_tcp_server(int port, FILE *input, FILE *output) {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    // Handle input and output redirection
+    if (input) {
+        dup2(new_socket, STDIN_FILENO);
+        handle_tcp_input(new_socket, output);
+    }
+    if (output) {
+        dup2(new_socket, STDOUT_FILENO);
+        handle_tcp_output(new_socket, input);
+    }
+
+    close(new_socket);
+    close(server_fd);
+}
+
+void start_tcp_client(const char *host, int port, FILE *input, FILE *output) {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, host, &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address / Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Handle input and output redirection
+    if (input) {
+        dup2(sock, STDIN_FILENO);
+        handle_tcp_input(sock, output);
+    }
+    if (output) {
+        dup2(sock, STDOUT_FILENO);
+        handle_tcp_output(sock, input);
+    }
+
+    close(sock);
+}
+
+int main(int argc, char *argv[]) {
+    // Check if bad syntax, exit(1)
+    if (argc < 5 || strcmp(argv[1], "-e") != 0) {
+        printf("Error: bad syntax\n");
+        fprintf(stderr, "Usage: %s -e <command> -i/-o/-b TCPS<PORT> | TCPC<IP,PORT>\n", argv[0]);
+        exit(1);
+    }
+
+    //For adding the ./ to run the program
+    char *run = "./";
+    int size = (sizeof(argv[2]) + sizeof(argv[3])) + 2;
+    char *cmd = (char*)malloc(sizeof(char)*size);
+    //Add the ./ befor the command
+    strcpy(cmd,run);
+    //Attach the command after the ./ run command
+    strcat(cmd,argv[2]);
+    strcat(cmd,argv[3]);
+
+    char *flag = argv[4];
+    char *param = argv[5];
+
+    // Execute the command with input/output redirection
+    if (strncmp(flag, "-i", 2) == 0 && strncmp(param, "TCPS", 4) == 0) {
+        int port = atoi(param + 4);
+        start_tcp_server(port, NULL, stdout);
+    } else if (strncmp(flag, "-o", 2) == 0 && strncmp(param, "TCPC", 4) == 0) {
+        char *host = strtok(param + 4, ",");
+        int port = atoi(strtok(NULL, ","));
+        start_tcp_client(host, port, stdin, NULL);
+    } else if (strncmp(flag, "-b", 2) == 0 && strncmp(param, "TCPS", 4) == 0) {
+        int port = atoi(param + 4);
+        start_tcp_server(port, stdin, stdout);
+    } else {
+        fprintf(stderr, "Unsupported flag or parameter\n");
+        exit(1);
+    }
+
+    return 0;
+}
+
+
